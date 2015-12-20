0f2d96a9ea95de84864f8ba6025ec12868f808f0
bhakyuz <bhakyuz@gmail.com>
2015-12-21 00:38:29 +0100

    initial commit as draft

---
+++ src/dbsa/main_app.java
@@ -0,0 +1,12 @@
+package dbsa;
+
+public class main_app {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		IO_1_Simple io1 = new IO_1_Simple();
+		io1.read_from_file();
+		
+	}
+
+}
