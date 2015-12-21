9c837a2d73bee2f0cfbc4376ca2307d9f8bc827a
bhakyuz <bhakyuz@gmail.com>
2015-12-21 15:33:25 +0100

    Changes in main app

--- src/dbsa/main_app.java
+++ src/dbsa/main_app.java
@@ -1,8 +1,27 @@
 package dbsa;
 
+import java.io.File;
+
 public class main_app {
 	public static void main(String[] args) {
+		// Generate files
 		GenerateData gd = new GenerateData();
 		gd.generateFilesToRead(2, 10, "src/resources/read_from");
+
+		// Read from the generated files
+		IO_1 io1 = new IO_1();
+		//io1.read_from_file("src/resources/read_from/generated_0.txt");
+
+		// Read from generated files and write to file
+		//io1.read_write_file("src/resources/read_from/generated_0.txt", "src/resources/write_to.txt");
+
+		// Read from the generated files
+		IO_2 io2 = new IO_2();
+		io2.read_from_file("src/resources/read_from/generated_0.txt");
+
+		// Read from generated files and write to file
+		io2.read_write_file("src/resources/read_from/generated_0.txt", "src/resources/write_to.txt");
+
+
 	}
 }
