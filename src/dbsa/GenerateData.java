2e4cbf183e3e6c8f01d58354134decfd9f00e60c
bhakyuz <bhakyuz@gmail.com>
2015-12-22 02:31:51 +0100

    tiny changes in data generator

--- src/dbsa/GenerateData.java
+++ src/dbsa/GenerateData.java
@@ -15,12 +15,12 @@ public class GenerateData {
 		Random rand = new Random();
 		String current_val;
 
-		for (int i = 0; i <= noOfFiles; i++) { // Generate noOfFiles files
+		for (int i = 1; i <= noOfFiles; i++) { // Generate noOfFiles files
 			String new_file = path + "/generated_" + Integer.toString(i) + ".txt";
 			Path file = Paths.get(new_file);
 			try (BufferedWriter writer = Files.newBufferedWriter(file, charset)) {
-				for (int j = 0; j <= noOfRows; j++) { // Generate noOfRows per each file
-					current_val = Integer.toString(rand.nextInt(1000));
+				for (int j = 1; j <= noOfRows; j++) { // Generate noOfRows per each file
+					current_val = Integer.toString(rand.nextInt(1000000000));
 					writer.write(current_val);
 					writer.newLine();
 				}
