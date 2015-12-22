2a73d9c522235faca8d8be0f20ace514f8c0b9b6
bhakyuz <bhakyuz@gmail.com>
2015-12-22 02:32:20 +0100

    Add method 3 and 4, change in mainApp

--- src/dbsa/main_app.java
+++ src/dbsa/main_app.java
@@ -17,11 +17,24 @@ public class main_app {
 
 		// Read from the generated files
 		IO_2 io2 = new IO_2();
-		io2.read_from_file("src/resources/read_from/generated_0.txt");
+		//io2.read_from_file("src/resources/read_from/generated_0.txt");
 
 		// Read from generated files and write to file
-		io2.read_write_file("src/resources/read_from/generated_0.txt", "src/resources/write_to.txt");
+		//io2.read_write_file("src/resources/read_from/generated_0.txt", "src/resources/write_to.txt");
 
+		// Read from the generated files
+		IO_3 io3 = new IO_3();
+		//io3.read_from_file("src/resources/read_from/generated_0.txt", 10);
+
+		// Read from generated files and write to file
+		//io3.read_write_file("src/resources/read_from/generated_1.txt", "src/resources/write_to2.txt",10);
+
+		// Read from the generated files
+		IO_4 io4 = new IO_4();
+		//io4.read_from_file("src/resources/read_from/generated_0.txt", 10);
+
+		// Read from generated files and write to file
+		//io4.read_write_file("src/resources/read_from/generated_0.txt", "src/resources/write_to.txt",20);
 
 	}
 }
